778b05654bfad5ca993ef0b8d5db9af3a6382af6
Xconvert <2808313820@qq.com>
2018-05-15 08:40:00 +0800

    函数指针

--- C++学习笔记.cpp
+++ C++学习笔记.cpp
@@ -1,7 +1,7 @@
 #include <iostream>  
 using namespace std;
 
-/*提高代码适用性，参数传递函数的指针*/
+/*||提高代码适用性，参数传递函数的指针*/
 int addition(int x, int y) {
 	return x + y;
 }
@@ -15,7 +15,11 @@ bool operation(int x, bool(*f)(int)) {//用指针将函数作为参数传参,函数指针
 	//or another return way: return (*f)(x);
 	return f(x);
 }
-int main() {
+void testisEven() {
 	bool answer = operation(1, isEven);
 	cout << answer;
+}
+/*提高代码适用性，参数传递函数的指针||*/
+int main() {
+	testisEven();
 }
